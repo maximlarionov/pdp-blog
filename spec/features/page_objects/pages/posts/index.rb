6f0761026f70ab5d3b5d6e824ea2a629c17a5c01
maximlarionov <maxim.larionov@flatstack.com>
2015-03-12 19:38:22 +0300

    split feature tests to user and visitor

--- spec/features/page_objects/pages/posts/index.rb
+++ spec/features/page_objects/pages/posts/index.rb
@@ -1,5 +1,8 @@
 module Posts
   class Index < SitePrism::Page
+    include Formulaic::Dsl
+    include FactoryGirl::Syntax::Methods
+
     set_url '/'
 
   end
---
+++ spec/features/visitor/posts/index_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+feature 'See Posts' do
+  let(:user) { create(:user, :confirmed) }
+  let(:another_user) { create(:user, :confirmed) }
+
+  let!(:post) { create(:post, user: user, title: "My Post") }
+  let!(:my_private_post) { create(:private_post, user: user, title: "Others don't see") }
+  let!(:another_private_post) { create(:private_post, user: another_user, title: "U don't see") }
+
+  before do
+    visit posts_path
+  end
+
+  scenario "I see posts#index page" do
+    expect(current_path).to eq posts_path
+    expect(page).to have_content("My Post")
+  end
+
+  scenario "And I don't see users private posts" do
+    expect(page).not_to have_content("U don't see")
+    expect(page).not_to have_content("Others don't see")
+  end
+
+  scenario "And I don't see edit/delete buttons" do
+    within("#post_#{post.id}") do
+      within('.button-group') do
+        expect(page).not_to have_content('Edit')
+        expect(page).not_to have_content('Destroy')
+      end
+    end
+  end
+
+  describe ".pagination" do
+    context "when there are more than 3 posts" do
+      let!(:posts) { create_list(:post, 4, user: user) }
+
+      scenario "I can see pagination" do
+        visit posts_path
+
+        within('.paginator') do
+          expect(page).to have_css(".pagination")
+        end
+      end
+    end
+
+    context "when there are 3 or less posts" do
+      scenario "I don't see pagination" do
+        within('.paginator') do
+          expect(page).not_to have_css(".pagination")
+        end
+      end
+    end
+  end
+end

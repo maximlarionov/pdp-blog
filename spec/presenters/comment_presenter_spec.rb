8b9f53a05fc6f9b075958bbc41a687694fb33b14
Maxim Larionov <maxim.larionov@flatstack.com>
2015-03-20 00:48:44 +0300

    Introduce Presenters for Posts and Comments

--- spec/presenters/comment_presenter_spec.rb
+++ spec/presenters/comment_presenter_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe CommentDecorator do
+describe CommentPresenter do
   let(:user) { create(:user, full_name: 'Obi Van Kenobi') }
   let(:post) { create(:post) }
   let(:comment) { create(:comment, user: user) }
-  let(:decorator) { comment.decorate }
+  let(:presenter) { CommentPresenter.wrap(comment) }
 
   context 'when decorating comment' do
     before { comment.created_at = 1.day.ago }
-    subject { decorator }
+    subject { presenter }
 
     its(:time) { is_expected.to be == comment.created_at.strftime('%d %b at %I:%M %p') }
     its(:author) { is_expected.to be == 'Obi Van Kenobi' }

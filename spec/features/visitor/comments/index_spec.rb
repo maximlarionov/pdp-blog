eee5ace0997a3359daca365f2bd484e8ffb7f45b
maximlarionov <maxim.larionov@flatstack.com>
2015-04-13 19:16:58 +0300

    update post presenter spec

--- spec/features/visitor/comments/index_spec.rb
+++ spec/features/visitor/comments/index_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 feature 'See Comments' do
-  let(:post) { create(:post) }
-  let(:comment) { create(:comment, post: post, message: "Hello there!")}
+  let!(:post) { create(:post) }
+  let!(:comment) { create(:comment, post: post, message: 'Hello there!') }
 
   before do
     visit feed_path
   end
 
   scenario 'I see comments on posts#feed page' do
-    expect(page).to have_content("Hello there!")
+    expect(page).to have_content('Hello there!')
   end
 end
--- spec/policies/access_policy_spec.rb
+++ spec/policies/access_policy_spec.rb
@@ -1,18 +1,3 @@
-# class AccessPolicy
-#   attr_reader :item, :requestor
-
-#   def initialize(item, requestor)
-#     @item, @requestor = item, requestor
-#   end
-
-#   def can_manage?
-#     item.user == requestor
-#   end
-
-#   def can_read?
-#     item.published? || can_manage?
-#   end
-# end
 require 'rails_helper'
 
 describe AccessPolicy do

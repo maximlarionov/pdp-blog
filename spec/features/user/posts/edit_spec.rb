032058b6acd408ddaf768c5a72bbd89f152d3bee
maximlarionov <maxim.larionov@flatstack.com>
2015-04-13 12:02:57 +0300

    Fix transaction issue - will open PR to rails-base

--- spec/features/user/posts/edit_spec.rb
+++ spec/features/user/posts/edit_spec.rb
@@ -1,39 +1,39 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# feature 'Edit Posts', js: true do
-#   let(:user) { create(:user, :confirmed) }
-#   let(:another_user) { create(:user, :confirmed) }
+feature 'Edit Posts', js: true do
+  let(:user) { create(:user, :confirmed) }
+  let(:another_user) { create(:user, :confirmed) }
 
-#   let!(:post) { create(:post, user: user, title: 'My Post') }
-#   let!(:another_post) { create(:post, user: another_user, title: 'His post') }
+  let!(:post) { create(:post, user: user, title: 'My Post') }
+  let!(:another_post) { create(:post, user: another_user, title: 'His post') }
 
-#   before do
-#     login_as user
+  before do
+    login_as user
 
-#     visit posts_path
-#   end
+    visit posts_path
+  end
 
-#   scenario 'edit with correct data' do
-#     within(".index-buttons") do
-#       click_link 'Edit'
-#     end
+  scenario 'edit with correct data' do
+    within('.index-buttons') do
+      click_link 'Edit'
+    end
 
-#     fill_in 'Title', with: 'New Title'
-#     click_button 'Update Post'
+    fill_in 'Title', with: 'New Title'
+    click_button 'Update Post'
 
-#     expect(page).to have_content('New Title')
-#   end
+    expect(page).to have_content('New Title')
+  end
 
-#   scenario 'edit with incorrect data' do
-#     within(".index-buttons") do
-#       click_link 'Edit'
-#     end
+  scenario 'edit with incorrect data' do
+    within('.index-buttons') do
+      click_link 'Edit'
+    end
 
-#     fill_in 'Title', with: ''
-#     fill_in 'Body', with: ''
+    fill_in 'Title', with: ''
+    fill_in 'Body', with: ''
 
-#     click_button 'Update Post'
+    click_button 'Update Post'
 
-#     expect(page).to have_content('Please review the problems below')
-#   end
-# end
+    expect(page).to have_content('Please review the problems below')
+  end
+end
